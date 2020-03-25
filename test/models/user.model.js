bc86273c3114a479ae048d2d9d9258b3652dad6a
Ola Mohammad AlYamani <49082678+OLA-ALYAMANI@users.noreply.github.com>
2020-03-25 19:53:06 +0300

    Update user.model.js

--- test/models/user.model.js
+++ test/models/user.model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
-const bcrypt = require("bcrypt")
-const salt = 10;
+const bcrypt = require("bcrypt")//تشفير كلمة المرور
+const salt = 10;// عدد العمليات الحسابية
 
 
 const userSchema = mongoose.Schema({
     firstname: {
         type: String,
-        required: true,
+        required: true,//لجعله أمر ضروري اي يجب ان يدخل لسم المستخدم
         // minlength: [
         //   3,
         //   "Aha! you are not real! First Name must be more than 3 characters"
