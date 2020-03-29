e53d842cf220ecc9eceb3cf8300cc22e5c6914b0
Ola Mohammad AlYamani <49082678+OLA-ALYAMANI@users.noreply.github.com>
2020-03-29 17:14:22 +0300

    Update user.model.js

--- test/models/user.model.js
+++ test/models/user.model.js
@@ -6,7 +6,7 @@ const salt = 10;// عدد العمليات الحسابية
 const userSchema = mongoose.Schema({
     firstname: {
         type: String,
-        required: true,//لجعله أمر ضروري اي يجب ان يدخل لسم المستخدم
+        required: true,//لجعله أمر ضروري اي يجب ان يدخل إسم المستخدم
         // minlength: [
         //   3,
         //   "Aha! you are not real! First Name must be more than 3 characters"
